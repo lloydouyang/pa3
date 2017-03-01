7b1b477229d81db3c4c14660a9b88df7a693fbf0
lloydouyang <do5xb@virginia.edu>
2017-03-01 01:37:04 -0500

    6

--- when_court.py
+++ when_court.py
@@ -18,7 +18,7 @@ def accessDatabase(year,month,day,start, end):
             a = [True] * 7
             for i in range(0,numrows):
                 if (result[i][1].year==year and result[i][1].month==month and result[i][1].day==day) :
-                    if ((result[i][2]<start and result[i][3]>end) or (result[i][2]<end and result[i][3]>end) or (result[i][2]<start and result[i][3]>start)):
+                    if ((str(result[i][2])<start and str(result[i][3])>end) or (str(result[i][2])<end and str(result[i][3])>end) or (str(result[i][2])<start and str(result[i][3])>start)):
                         a[result[i][0]]=False
             for k in range(1,7):
                 if (a[k]==True) :
