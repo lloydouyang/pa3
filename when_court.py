cde32f6448c6219cf3a86a73685571f3747a6188
lloydouyang <do5xb@virginia.edu>
2017-03-01 01:34:02 -0500

    6

--- when_court.py
+++ when_court.py
@@ -15,14 +15,14 @@ def accessDatabase(year,month,day,start, end):
             result = cursor.fetchall()
             numrows = len(result)    
             
-            a = [True] * 6
+            a = [True] * 7
             for i in range(0,numrows):
                 if (result[i][1].year==year and result[i][1].month==month and result[i][1].day==day) :
                     if ((result[i][2]<start and result[i][3]>end) or (result[i][2]<end and result[i][3]>end) or (result[i][2]<start and result[i][3]>start)):
                         a[result[i][0]]=False
-            for k in range(0,5):
+            for k in range(1,7):
                 if (a[k]==True) :
-                    print(k+1)
+                    print(k)
 
     finally:
         connection.close()
